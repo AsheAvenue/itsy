0c808c9f62990fd9f018bc6e3d468bd35713c7fa
Heath Beckett <heath@asheavenue.com>
2014-01-15 10:02:15 -0500

    adding itsy.js with base functions: getId(), getTag(), getClass(), each()

---
+++ itsy.js
@@ -0,0 +1,57 @@
+/*!
+ * itsy.js 0.0.1
+ * https://github.com/AsheAvenue/itsy
+ *
+ * Copyright 2014 Ashe Avenue. Created by Heath Beckett and Tim Boisvert.
+ * Released under the MIT license
+ */
+
+var itz = function() {};
+
+itz.version    = '0.0.1';
+
+itz.ArrayProto = Array.prototype;
+
+itz.getId = function(id) {
+    if (id == null) return;
+    return document.getElementById(id);
+}
+
+itz.getTag = function(tagName) {
+    if (tagName == null) return;
+    return document.getElementsByTagName(tagName);
+};
+
+itz.getClass = function(className) {
+    var els = document.getElementsByTagName('*'),
+        returnEls = [],
+        i;
+    for (i in els) {
+        if ((' ' + els[i].className + ' ').indexOf(' ' + className + ' ') > -1) {
+            returnEls.push(els[i]);
+        }
+    }
+    if (returnEls.length) {
+        return returnEls;
+    } else {
+        return false;
+    }
+};
+
+itz.each = function(obj, iterator, context) {
+    if (obj == null) return;
+    if (obj.length === +obj.length) {
+        for (var i = 0, length = obj.length; i < length; i++) {
+            if (iterator.call(context, obj[i], i, obj) === {}) return;
+        }
+    } else {
+        var keys = _.keys(obj);
+        for (var i = 0, length = keys.length; i < length; i++) {
+            if (iterator.call(context, obj[keys[i]], keys[i], obj) === {}) return;
+        }
+    }
+};
+
+// todo: itz.post
+
+// todo: itz.get
